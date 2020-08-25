6ceebf6cf833985232bf895ad02aac6c9b4f407a
Artem Goncharov <Artem.goncharov@gmail.com>
2020-08-25 20:57:19 +0200

    small update

--- tests/utils/integration.sh
+++ tests/utils/integration.sh
@@ -17,22 +17,22 @@ NAMESPACE="$1"; shift
 NAME="$1"; shift
 TEST_DIR="${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/${NAMESPACE}/${NAME}"
 
-trap "rm -rf ${ANSIBLE_COLLECTIONS_PATH}" err exit
+trap 'rm -rf ${ANSIBLE_COLLECTIONS_PATH}' err exit
 
 rm -rf "${ANSIBLE_COLLECTIONS_PATH}"
 
-mkdir -p $TEST_DIR
+mkdir -p "$TEST_DIR"
 
 rsync -av . \
     --exclude tests/output \
     --exclude .tox \
     --exclude .git \
-    $TEST_DIR > /dev/null|| true
+    "$TEST_DIR" > /dev/null|| true
 
 if [ -f "requirements.txt" ]; then
-    ansible-galaxy collection install -p ${ANSIBLE_COLLECTIONS_PATH}
-    ansible-galaxy role install -p ${ANSIBLE_COLLECTIONS_PATH}
+    ansible-galaxy collection install -p "${ANSIBLE_COLLECTIONS_PATH}"
+    ansible-galaxy role install -p "${ANSIBLE_COLLECTIONS_PATH}"
 fi
 
-cd $TEST_DIR
+cd "$TEST_DIR"
 ansible-test integration
--- tests/utils/sanity.sh
+++ tests/utils/sanity.sh
@@ -7,11 +7,11 @@ NAMESPACE="$1"; shift
 NAME="$1"; shift
 TEST_DIR="${ANSIBLE_COLLECTIONS_PATH}/ansible_collections/${NAMESPACE}/${NAME}"
 
-trap "rm -rf ${ANSIBLE_COLLECTIONS_PATH}" err exit
+trap 'rm -rf ${ANSIBLE_COLLECTIONS_PATH}' err exit
 
 rm -rf "${ANSIBLE_COLLECTIONS_PATH}"
 
-mkdir -p $TEST_DIR
+mkdir -p "$TEST_DIR"
 
 rsync -av . \
     --exclude tests/output \
@@ -19,12 +19,12 @@ rsync -av . \
     --exclude ci \
     --exclude .tox \
     --exclude .git \
-    $TEST_DIR > /dev/null|| true
+    "$TEST_DIR" > /dev/null|| true
 
 if [ -f "requirements.txt" ]; then
     ansible-galaxy collection install -p ${ANSIBLE_COLLECTIONS_PATH}
     ansible-galaxy role install -p ${ANSIBLE_COLLECTIONS_PATH}
 fi
 
-cd $TEST_DIR
+cd "$TEST_DIR"
 ansible-test sanity --skip-test metaclass-boilerplate --skip-test future-import-boilerplate 
