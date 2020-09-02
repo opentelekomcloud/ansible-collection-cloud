d8b794e7ac143be259b4715dcad1da7a07716e6a
T. Schreiber <tino.schreiber@t-systems.com>
2020-09-02 08:32:19 +0000

    nat_gateway_info finished

--- plugins/modules/nat_gateway_info.py
+++ plugins/modules/nat_gateway_info.py
@@ -12,7 +12,6 @@
 # limitations under the License.
 
 DOCUMENTATION = '''
----
 module: nat_gateway_info
 short_description: Get NAT gateways
 extends_documentation_fragment: opentelekomcloud.cloud.otc
@@ -20,23 +19,100 @@ version_added: "0.0.1"
 author: "Tino Schreiber (@tischrei)"
 description:
   - Get NAT gateway info from the OTC.
+options:
+  admin_state_up:
+    description:
+      - NAT gateway state.
+    type: bool
+  created_at:
+    description:
+      - Creation time of the NAT gateway
+    type: str
+  description:
+    description:
+      - Description of the NAT gateway
+    type: str
+  id:
+    description:
+      - ID of the NAT gateway.
+    type: str
+  internal_network_id:
+    description:
+      - Network ID where the NAT gateway is attached to.
+    type: str
+  name:
+    description:
+      - Name of the NAT gateway.
+    type: str
+  project_id:
+    description:
+      - Filters NAT gateways of the project ID.
+    type: str
+  router_id:
+    description:
+      - ID of the router where the NAT gateway is attached.
+    type: str
+  spec:
+    description:
+      - Specifies the type of the NAT gateway: 1 (small: 10.000 connections),
+        2 (medium: 50.000 connections), 3 (large: 200.000 connections), 4
+        (extra-large: 1.000.000 connections)
+    type: str
+    choices: [1, 2, 3, 4]
+  status:
+    description:
+      - Specifies the status of the NAT gateway
+    type: str
+
 requirements: ["openstacksdk", "otcextensions"]
 '''
 
 RETURN = '''
-as_configs:
+nat_gateways:
     description: List of dictionaries describing NAT gateways.
     type: complex
     returned: On Success.
     contains:
+        admin_state_up:
+            description: Specifies whether gateway is up or down.
+            type: bool
+            sample: True
+        created_at:
+            description: Creation time of the NAT gateway
+            type: str
+            sample: "yyyy-mm-dd hh:mm:ss"
+        description:
+            description: Description of the NAT gateway
+            type: str
+            sample: "My Gateway"
         id:
             description: Unique UUID.
             type: str
             sample: "39007a7e-ee4f-4d13-8283-b4da2e037c69"
-        name:
-            description: Name (version) of the datastore.
+        internal_network_id:
+            description: Network ID where the NAT gateway is attached to.
             type: str
-            sample: "10.0"
+            sample: "25d24fc8-d019-4a34-9fff-0a09fde6a123"
+        name:
+            description: Name of the gateway.
+            type: str
+            sample: "nat-1234"
+        project_id:
+            description: Project ID where the NAT gateway is located in.
+            type: str
+            sample: "16d53a84a13b49529d2e2c3646612345"
+        router_id:
+            description: VPC / Router ID where the NAT gateway is attached to.
+            type: str
+            sample: "25d24fc8-d019-4a34-9fff-0a09fde6a123"
+        spec:
+            description: Specifies the type of the NAT gateway.
+            type: str
+            sample: "1"
+        status:
+            description: Specifies the status of the NAT gateway.
+            type: str
+            sample: "ACTIVE"
 '''
 
 EXAMPLES = '''
@@ -44,9 +120,19 @@ EXAMPLES = '''
 - nat_gateway_info:
   register: gw
 
-- as_config_info:
-    name: my_gateway
+- nat_gateway_info:
+    name: "my_gateway"
   register: gw
+
+- nat_gateway_info:
+    spec: "1"
+  register: gw
+
+- nat_gateway_info:
+    status: "ACTIVE"
+    spec: "1"
+  register: gw
+
 '''
 
 from ansible_collections.opentelekomcloud.cloud.plugins.module_utils.otc import OTCModule
@@ -56,7 +142,7 @@ class NATGatewayInfoModule(OTCModule):
     argument_spec = dict(
         name=dict(required=False),
         id=dict(required=False),
-        admin_state_up=dict(required=False),
+        admin_state_up=dict(required=False, type=bool, default=True),
         created_at=dict(required=False),
         description=dict(required=False),
         internal_network_id=dict(required=False),
