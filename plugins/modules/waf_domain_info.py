da5490ebaa5edf03534c6451bbef7e2f6f1783c7
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-09-02 13:05:53 +0300

    stub for waf_domain_info

---
+++ plugins/modules/waf_domain_info.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: waf_domain_info
+short_description: Get WAF domain info
+extends_documentation_fragment: opentelekomcloud.cloud.otc
+version_added: "0.0.3"
+author: "Anton Sidelnikov (@anton-sidelnikov)"
+description:
+  - Get WAF Domain info from the OTC or list all domains.
+options:
+  test:
+    description: for future use
+    type: str
+requirements: ["openstacksdk", "otcextensions"]
+'''
+
+RETURN = '''
+waf_domain_info:
+  description: List of dictionaries describing domains matching query.
+  type: complex
+  returned: On Success.
+  contains:
+    id:
+      description: Specifies the instance ID.
+      type: str
+    hostname:
+      description: Specifies the domain name.
+      type: str
+    cname:
+      description: Specifies the CNAME value.
+      type: str
+      sample: "efec1196267b41c399f2980ea4048517.waf.cloud.com."
+    policy_id:
+      description: Specifies the policy ID.
+      type: str
+    protect_status:
+      description: Specifies the WAF mode.
+      type: int
+    access_status:
+      description: Specifies whether a domain name is connected to WAF.
+      type: int
+    protocol:
+      description: Specifies the protocol type.
+      type: str
+    certificate_id:
+      description: Specifies the certificate ID.
+      type: str
+    server:
+      description: Specifies the origin server information.
+      type: dict
+    proxy:
+      description: Specifies whether a proxy is configured.
+      type: bool
+    timestamp:
+      description: Specifies the time when a domain name is created.
+      type: str
+'''
+
+EXAMPLES = '''
+# Get Domain.
+- waf_domain_info:
+  register: domain
+'''
+
+from ansible_collections.opentelekomcloud.cloud.plugins.module_utils.otc import OTCModule
+
+
+class WafDomainInfoModule(OTCModule):
+    argument_spec = dict(
+        name=dict(required=False),
+    )
+
+    otce_min_version = '0.8.0'
+
+    def run(self):
+
+        data = []
+
+        for raw in self.conn.waf.find_domain(offset=0, limit=-1):
+            dt = raw.to_dict()
+            dt.pop('location')
+            data.append(dt)
+
+        self.exit(
+            changed=False,
+            waf_domains=data
+        )
+
+
+def main():
+    module = WafDomainInfoModule()
+    module()
+
+
+if __name__ == '__main__':
+    main()
