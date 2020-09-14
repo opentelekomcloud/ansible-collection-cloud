7e0ab224f03de21427c7bdbfd797c19499d4b433
Polina-Gubina <Polina.Gubina@t-systems.com>
2020-09-14 11:05:08 +0300

    Change 'version_added'

--- plugins/modules/vpc_peering_info.py
+++ plugins/modules/vpc_peering_info.py
@@ -16,29 +16,33 @@ DOCUMENTATION = '''
 module: vpc_peering_info
 short_description: Get information about vpc peerings
 extends_documentation_fragment: opentelekomcloud.cloud.otc
-version_added: "0.0.3"
+version_added: "0.0.4"
 author: "Polina Gubina (@polina-gubina)"
 description:
   - Get a generator of vpc peerings info from the OTC.
 options:
   name:
-    description: Peering connection name.
+    description:
+      - Peering connection name.
     type: str
   status:
-    description: Peering connection status.
-    type: str
+    description:
+      - Peering connection status.
     choices: [PENDING_ACCEPTANCE, REJECTED, EXPIRED, DELETED, ACTIVE]
+    type: str
   tenant_id:
-    description: Tenant ID.
+    description:
+      - Tenant ID.
     type: str
   vpc_id:
-    description: VPC ID.
+    description:
+      - VPC ID.
     type: str
 requirements: ["openstacksdk", "otcextensions"]
 '''
 
 RETURN = '''
-peerings:
+vpc_peerings:
   description: The VPC peering connection object list.
   type: complex
   returned: On Success.
@@ -56,19 +60,13 @@ peerings:
       type: str
       sample: "ACTIVE"
     request_vpc_info:
-      description: 	Information about the local VPC.
+      description: Information about the local VPC.
       type: dict
-      sample: "{
-                "tenant_id": "76889f64a23945ab887012be95acf",
-                "vpc_id": "4dae5bac-0925-4d5b-add8-cb6667b8"
-                }"
+      sample: "{tenant_id: 76889f64a23945ab887012be95acf, vpc_id: 4dae5bac-0925-4d5b-add8-cb6667b8}"
     accept_vpc_info:
-      description:  Information about the peer VPC.
+      description: Information about the peer VPC.
       type: dict
-      sample: "{
-                "tenant_id": "968u64a23945ab887012be95acf",
-                "vpc_id": "7dau5bac-0925-4d5b-add8-cb6667b8"
-                }"
+      sample: "{tenant_id: 968u64a23945ab887012be95acf, vpc_id: 7dau5bac-0925-4d5b-add8-cb6667b8}"
     description:
       description: Provides supplementary information about the VPC peering connection.
       type: str
@@ -85,7 +83,7 @@ peerings:
 
 EXAMPLES = '''
 # Get configs versions.
-- tasks:
+- vpc_peering_info:
     name: vpc_peering1
   register: vpc_peering
 '''
@@ -96,7 +94,7 @@ from ansible_collections.opentelekomcloud.cloud.plugins.module_utils.otc import 
 class VPCPeeringInfoModule(OTCModule):
     argument_spec = dict(
         name=dict(required=False),
-        status=dict(required=False),
+        status=dict(required=False, choices=['PENDING_ACCEPTANCE', 'REJECTED', 'EXPIRED', 'DELETED', 'ACTIVE']),
         tenant_id=dict(required=False),
         vpc_id=dict(required=False),
     )
