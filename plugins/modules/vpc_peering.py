cc89a172edcf2ca0e1cef7d8c7385451c6db3e0c
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-09-25 16:41:49 +0300

    fix line break after binary operator

--- plugins/modules/vpc_peering.py
+++ plugins/modules/vpc_peering.py
@@ -176,10 +176,8 @@ class VPCPeeringModule(OTCModule):
 
         if peerings:
             for peering in peerings:
-                if (peering['local_vpc_info']['vpc_id'] == local_vpc_id and
-                   peering['peer_vpc_info']['vpc_id'] == peer_vpc_id) or\
-                   (peering['local_vpc_info']['vpc_id'] == peer_vpc_id and
-                   peering['peer_vpc_info']['vpc_id'] == local_vpc_id):
+                if (peering['local_vpc_info']['vpc_id'] == local_vpc_id and peering['peer_vpc_info'][ 'vpc_id'] == peer_vpc_id) or \
+                        (peering['local_vpc_info']['vpc_id'] == peer_vpc_id and peering['peer_vpc_info']['vpc_id'] == local_vpc_id):
                     result = False
 
         return result
