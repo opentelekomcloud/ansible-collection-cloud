02c1f29a68428d86684e18467d401f979f9bfb19
Sebastian Gode <sebastian.gode@telekom.de>
2020-11-10 09:11:20 +0000

    First semi working zone script

--- plugins/modules/dns_zones.py
+++ plugins/modules/dns_zones.py
@@ -124,8 +124,9 @@ from ansible_collections.opentelekomcloud.cloud.plugins.module_utils.otc import 
 class DNSRecordsetModule(OTCModule):
     argument_spec = dict(
         description=dict(required=False),
-        email=dict(required=True, type='list'),
+        email=dict(required=False),
         name=dict(required=True),
+        router=dict(required=False),
         state=dict(type='str', choices=['present', 'absent'], default='present'),
         ttl=dict(required=False, type='int'),
         zone_type=dict(required=True)
@@ -134,34 +135,71 @@ class DNSRecordsetModule(OTCModule):
     def run(self):
         changed = False
         data = []
+        query = {}
 
-        zo = self.conn.dns.find_zone(
-            name_or_id=self.params['name'],
-            ignore_missing=True
-        )
-
-        if self.params['state'] == 'absent':
-            changed = False
+        # find_zone doesn't support searching for private zones with name
+        # As a result we use it only to search in public zones
+        if self.params['zone_type'] == 'public':
+            zo = self.conn.dns.find_zone(
+                name_or_id=self.params['name'],
+                ignore_missing=True
+            )
             if zo:
-                self.conn.dns.delete_zone(
-                    zone=zo.id
-                )
-                changed = True
-            if not rs:
+                zone_id = zo.id
+            else:
                 self.exit(
                     changed=False,
                     message=('No Zone found with name: %s' %
                              self.params['name'])
                 )
 
+
+        # For private Zones we list all zones and then filter by name
+        if self.params['zone_type'] == 'private':
+            query['zone_type'] = self.params['zone_type']
+            for raw in self.conn.dns.zones(**query):
+                dt = raw.to_dict()
+                dt.pop('location')
+                data.append(dt)
+            # Query parameter name is not supported, so we need to filter it by hand
+            i = 0
+            while i < len(data):
+                if data[i]['name'] != self.params['name']:
+                    del data[i]
+                    i = 0
+                    continue
+                i = i+1
+            # As we remove datasets we need to check wether there's the correct one left to obtain the ID
+            if len(data) != 0:    
+                zone_id = data[0]['id']
+            else:
+                self.exit(
+                    changed=False,
+                    message=('No Zone found with name: %s' %
+                             self.params['name'])
+                )
+
+        # We now have the zone_id to work with
+        if self.params['state'] == 'absent':
+            changed = False
+            # No check for zone_id necessary as we checked it
+            self.conn.dns.delete_zone(
+                zone=zone_id
+            )
+            changed = True
+
         if self.params['state'] == 'present':
             attrs = {}
-            rs = self.conn.dns.find_recordset(
-                name_or_id=self.params['recordset_name'],
-                zone=zo.id,
-                ignore_missing=True
-            )
-
+            # Check if VPC exists
+            if self.params['zone_type'] == 'private':
+                ro = self.conn.network.find_router(
+                    name_or_id=self.params['router'],
+                    ignore_missing=True
+                )
+            if self.params['description']:
+                attrs['description'] = self.params['description']
+            if self.params['email']:
+                attrs['email'] = self.params['email']
             if not rs:
                 attrs['name'] = self.params['recordset_name']
                 if self.params['description']:
