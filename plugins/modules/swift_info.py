677d60021360daf40f5c9da9a530647e51002b8c
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-02-20 13:04:40 +0300

    added returns

--- plugins/modules/swift_info.py
+++ plugins/modules/swift_info.py
@@ -33,11 +33,85 @@ requirements: ["openstacksdk", "otcextensions"]
 '''
 
 RETURN = '''
-swift_content:
-    description: List of dictionaries describing AutoScaling Configs.
-    type: complex
-    returned: On Success.
-    contains:
+if container and object:
+swift:
+  description: List of dictionaries describing object content.
+  type: complex
+  returned: On Success.
+  contains:
+    content:
+      description: Object content.
+      type: str
+      sample: "test\n"
+
+if container:
+swift:
+  description: List of dictionaries describing objects in container.
+  type: complex
+  returned: On Success.
+  contains:
+    objects:
+      type: list
+      elements: dict
+        name:
+          description: Name of the object.
+          type: str
+        content_type:
+          description: Content type of the object.
+          type: str
+        _bytes:
+          description: Total number of bytes that are stored in OBS for the
+            object.
+          type: int
+        _hash:
+          description: MD5 checksum value of the object content.
+          type: str
+          sample: "e1cbb0c3879af8347246f12c559a86b5"
+        accept_ranges:
+          description: Type of ranges that the object accepts.
+          type: str
+        etag:
+          description: For ordinary objects, this value is
+            the MD5 checksum of the object content.
+            For manifest objects, this value is the MD5 checksum of the
+            concatenated string of MD5 checksums for each of the segments in
+            the manifest.
+          type: str
+          sample: "e1cbb0c3879af8347246f12c559a86b5"
+        last_modified_at:
+          description: Modification time and date
+          type: str
+          sample: "2021-02-18T13:40:09.640760"
+        timestamp:
+          description: Creation time and date
+          type: str
+          sample: "2021-02-18T13:40:09.640760"
+
+swift:
+  description: List of dictionaries describing containers.
+  type: complex
+  returned: On Success.
+  contains:
+    containers:
+      type: list
+      elements: dict
+        bytes:
+          description: Total number of bytes that are stored in OBS for the
+            object.
+          type: int
+        content_type:
+          description: Content type of the object.
+          type: str
+        count:
+          description: Count of objects in container
+          type: int
+        name:
+          description: Name of the container
+          type: str
+        timestamp:
+          description: Creation time and date
+          type: str
+          sample: "2021-02-18T13:40:09.640760"
 '''
 
 EXAMPLES = '''
@@ -67,19 +141,19 @@ class SwiftInfoModule(OTCModule):
         container = self.params['container']
         object_name = self.params['object_name']
         if container and object_name:
-            content = self.conn.download_object(object_name, container)
+            content = self.conn.object_store.download_object(object_name, container)
             self.exit(changed=False, swift=dict(content=content))
 
         if container:
             objects = []
-            for raw in self.conn.objects(container):
+            for raw in self.conn.object_store.objects(container):
                 dt = raw.to_dict()
                 dt.pop('location')
                 objects.append(dt)
             self.exit(changed=False, swift=dict(objects=objects))
 
         containers = []
-        for raw in self.conn.containers():
+        for raw in self.conn.object_store.containers():
             dt = raw.to_dict()
             dt.pop('location')
             containers.append(dt)
