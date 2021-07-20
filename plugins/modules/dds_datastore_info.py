b1dd40f7a70fc26743b9d9223b49f362e16a3617
Yustina Kvrivishvili <iustina.kvrivishvili@t-systems.com>
2021-07-20 20:46:47 +0300

    fix ignore files

--- plugins/modules/dds_datastore_info.py
+++ plugins/modules/dds_datastore_info.py
@@ -1,4 +1,4 @@
-#
+#!/usr/bin/python
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,7 +12,7 @@ extends_documentation_fragment: opentelekomcloud.cloud.otc
 version_added: "0.3.0"
 author: "Yustina Kvrivishvili (@YustinaKvr)"
 description:
-  - Get Metric Data
+  - Get datastore info
 options:
   datastore_name:
     description:
