afc20e8e058847b335f51e70443c17bec92720a0
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-10-26 16:36:41 +0300

    version_added: "0.24.1"

--- plugins/modules/css_cluster_info.py
+++ plugins/modules/css_cluster_info.py
@@ -15,7 +15,7 @@ DOCUMENTATION = '''
 module: css_cluster_info
 short_description: Get info about CSS clusters.
 extends_documentation_fragment: opentelekomcloud.cloud.otc
-version_added: "0.8.1"
+version_added: "0.24.1"
 author: "Yustina Kvrivishvili (@YustinaKvr)"
 description:
   - Get CSS cluster info from the OTC.
