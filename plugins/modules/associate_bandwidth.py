7f501385b042d7ea8da3368d26d3322c4786c649
Polina-Gubina <polinagubina6@gmail.com>
2023-02-15 14:33:06 +0100

    add associate bandwidth module

--- plugins/modules/associate_bandwidth.py
+++ plugins/modules/associate_bandwidth.py
@@ -12,12 +12,12 @@
 # limitations under the License.
 
 DOCUMENTATION = '''
-module: bandwidth
-short_description: Manage VPC bandwidth
+module: associate_bandwidth
+short_description: Add or remove eip to shared bandwidth
 extends_documentation_fragment: opentelekomcloud.cloud.otc
 version_added: "0.13.1"
 author: "Gubina Polina (@Polina-Gubina)"
-description: Manage VPC bandwidth resource from the OTC.
+description: Add or remove eip to shared bandwidth
 options:
   name:
     description: Bandwidth name of id.
@@ -25,18 +25,34 @@ options:
     required: true
   size:
     description:
-      - Specifies the bandwidth size. The shared bandwidth has a minimum\
-      limit, which may vary depending on sites. The value ranges from 5 Mbit/s\
-      to 1000 Mbit/s by default. If a decimal fraction (for example 10.2) or\
-      a character string (for example "10") is specified, the specified value\
-      will be automatically converted to an integer. The minimum increment \
-      for bandwidth adjustment varies depending on the bandwidth range.\
-      The minimum increment is 1 Mbit/s if the allowed bandwidth ranges from\
-      0 Mbit/s to 300 Mbit/s (with 300 Mbit/s included). The minimum increment\
-      is 50 Mbit/s if the allowed bandwidth ranges from 300 Mbit/s to 1000\
-      Mbit/s (with 1000 Mbit/s included). The minimum increment is 500 Mbit/s\
-      if the allowed bandwidth is greater than 1000 Mbit/s.
+      - Specifies size which will be allocated for removed eip.\
+      After an EIP is removed from a shared bandwidth, a dedicated\
+      bandwidth will be allocated to the EIP, and you  will be billed\
+      for the dedicated bandwidth.
     type: int
+  publicip_id:
+    description:
+      - Specifies the ID of the EIP to be added or removed from\
+       shared bandwidth.
+    type: str
+    required: true
+  publicip_type:
+    description:
+      - Specifies the EIP type.
+      - publicip_id is an IPv4 port. If publicip_type is not specified, the \
+      default value is 5_bgp.
+    type: str
+    choices: ['5_bgp', '5_mailbgp', '5_gray']
+  charge_mode:
+    description:
+        - After an EIP is removed from a shared bandwidth, a dedicated\
+         bandwidth will be allocated to the EIP, and you will be billed for\
+         the dedicated bandwidth. Specifies whether the dedicated bandwidth\
+         used by the EIP that has been removed from a shared bandwidth is\
+         billed by traffic or by bandwidth.
+        - Mandatory for removing eip from a shared bandwidth.
+    type: str
+    choices: ['bandwidth', 'traffic']
   state:
     description: Whether resource should be present or absent.
     choices: [present, absent]
@@ -58,9 +74,8 @@ bandwidth:
       description: Specifies the bandwidth size.
       type: int
     id:
-      description:
-        - Specifies the bandwidth ID, which uniquely\
-         identifies the bandwidth.
+      description: Specifies the bandwidth ID, which uniquely\
+       identifies the bandwidth.
       type: str
     share_type:
       description:
@@ -111,19 +126,17 @@ bandwidth:
 '''
 
 EXAMPLES = '''
-# Assign bandwidth:
+# Add eip to bandwidth:
 opentelekomcloud.cloud.bandwidth:
   name: "test-bandwidth"
-  size: 15
+  publicip_id: "42d922af-18be-4e6d-804e-18f8c8871471"
 
-# Update bandwidth:
+# Remove eip from bandwidth:
 opentelekomcloud.cloud.bandwidth:
-  name: "test-bandwidth"
+  name: "new-bandwidth"
+  publicip_id: "42d922af-18be-4e6d-804e-18f8c8871471"
+  charge_mode: "traffic"
   size: 10
-
-# Delete bandwidth:
-opentelekomcloud.cloud.bandwidth:
-  name: "test-bandwidth"
   state: absent
 '''
 
@@ -134,27 +147,32 @@ class Bandwidth(OTCModule):
     argument_spec = dict(
         name=dict(required=True),
         size=dict(type='int', required=False),
-        state=dict(type='str', choices=['present', 'absent'], default='present')
+        publicip_id=dict(type='str', required=True),
+        publicip_type=dict(type='str',
+                           choices=['5_bgp', '5_mailbgp', '5_gray'],
+                           required=False),
+        charge_mode=dict(type='str',
+                         choices=['bandwidth', 'traffic'], required=False),
+        state=dict(type='str',
+                   choices=['present', 'absent'], default='present'),
     )
     module_kwargs = dict(
+        required_if=[
+            ('state', 'absent', ['charge_mode', 'size'])
+        ],
         supports_check_mode=True
     )
 
     def _system_state_change(self, bandwidth):
-        state = self.params['state']
         changed = False
-        if state == 'present' and not bandwidth:
+        publicip_id = self.params['publicip_id']
+        publicip_ids = [dict['publicip_id']
+                        for dict in bandwidth.publicip_info]
+        current = publicip_id in publicip_ids
+        if self.params['state'] == 'present' and not current:
             changed = True
-        elif state == 'absent' and bandwidth:
+        if self.params['state'] == 'absent' and current:
             changed = True
-        return changed
-
-    def _require_update(self, bandwidth):
-        changed = False
-        if bandwidth:
-            if self.params['size']:
-                if self.params['size'] != bandwidth['size']:
-                    return True
         return changed
 
     def run(self):
@@ -163,32 +181,38 @@ class Bandwidth(OTCModule):
         state = self.params['state']
         bandwidth = self.conn.vpc.find_bandwidth(
             name_or_id=self.params['name'])
+        if not bandwidth:
+            self.fail_json(
+                msg='bandwidth {0} not found'.format(self.params['name']))
 
+        changed = self._system_state_change(bandwidth)
         if self.ansible.check_mode:
-            changed = (self._system_state_change(bandwidth)
-                       or self._require_update(bandwidth))
             self.exit_json(changed=changed)
 
-        if not bandwidth:
-            if state == 'absent':
-                self.exit(changed=False)
-            query['name'] = self.params['name']
-            if self.params['size']:
-                query['size'] = self.params['size']
-            new_bandwidth = self.conn.vpc.assign_bandwidth(**query)
-            self.exit_json(changed=True, bandwidth=new_bandwidth)
+        if not changed:
+            self.exit_json(changed=False)
 
         query['bandwidth'] = bandwidth.id
 
-        if state == 'absent':
-            self.conn.vpc.delete_bandwidth(**query)
-            self.exit(changed=True)
-        if not self._require_update(bandwidth):
-            self.exit_json(changed=False)
-        if self.params['size']:
+        if state == 'present':
+            if self.params['publicip_type']:
+                publicip_type = self.params['publicip_type']
+            else:
+                publicip_type = '5_bgp'
+            query['publicip_info'] = [
+                {'publicip_id': self.params['publicip_id'],
+                 'publicip_type': publicip_type}
+            ]
+            new_bandwidth = self.conn.vpc.add_eip_to_bandwidth(**query)
+            self.exit_json(changed=True, bandwidth=new_bandwidth)
+        else:
+            query['charge_mode'] = self.params['charge_mode']
             query['size'] = self.params['size']
-        new_bandwidth = self.conn.vpc.update_bandwidth(**query)
-        self.exit_json(changed=True, bandwidth=new_bandwidth)
+            query['publicip_info'] = [
+                {'publicip_id': self.params['publicip_id']}
+            ]
+            new_bandwidth = self.conn.vpc.remove_eip_from_bandwidth(**query)
+            self.exit_json(changed=True, bandwidth=new_bandwidth)
 
 
 def main():
