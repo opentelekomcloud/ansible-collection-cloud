c882a325e2e02ff6d23acea8598dde6d3b98f960
Valeriia Ziukina <sand16031986@gmail.com>
2024-03-16 11:17:22 +0100

    auth not working

---
+++ plugins/modules/swr_repository_permissions.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DOCUMENTATION = '''
+---
+module: swr_repository_permissions
+short_description: Create, update or delete repository permissions in SWR
+extends_documentation_fragment: opentelekomcloud.cloud.otc
+version_added: "0.14.2"
+author: "Ziukina Valeriia (@RusselSand)"
+description:
+  - Create, update or delete repository permissions in Software Repository for Containers
+options:
+  namespace:
+    description: Mandatory name of organization.
+    type: str
+    required: true
+  repository:
+    description: Mandatory name of repository.
+    type: str
+    required: true
+  user_id:
+    description: User ID
+    type: str
+    required: true
+  user_name:
+    description: Username
+    type: str
+    required: true
+  user_auth:
+    description: User permission (7 — manage, 3 — write, 1 — read)
+    default: 1
+    choices: [1, 3, 7]
+    type: int
+  state:
+    description:
+      - Whether resource should be present or absent.
+    choices: ['present', 'absent']
+    type: str
+    default: 'present'
+requirements: ["openstacksdk", "otcextensions"]
+'''
+
+RETURN = '''
+permission:
+    description: Repository permission
+    type: complex
+    returned: On Success.
+    contains:
+      id:
+        description: Specifies the ID of the organization.
+        type: int
+      name:
+        description: Specifies the name of the organization.
+        type: str
+      auth:
+        description: User permission
+        type: int
+      creator_name:
+        description: Name of the creator og the organization
+        type: str
+'''
+
+EXAMPLES = '''
+# Create or delete SWR repository permission
+- name: Create new repository permission
+  opentelekomcloud.cloud.swr_organization:
+    namespace: org_name
+
+- name: Delete an repository permission
+  opentelekomcloud.cloud.swr_organization:
+    namespace: org_name
+    state: absent
+'''
+
+from ansible_collections.opentelekomcloud.cloud.plugins.module_utils.otc import OTCModule
+from ansible_collections.openstack.cloud.plugins.module_utils.resource import StateMachine
+
+
+class SwrRepoPermissionMachine(StateMachine):
+    def __call__(self, attributes, check_mode, state, timeout, wait,
+                 updateable_attributes, non_updateable_attributes, **kwargs):
+        # kwargs is for passing arguments to subclasses
+        resource = self._find(attributes, **kwargs)
+        if check_mode:
+            return self._simulate(state, resource, attributes, timeout, wait,
+                                  updateable_attributes,
+                                  non_updateable_attributes, **kwargs)
+
+        if state == 'present' and not resource:
+            # Create resource
+            resource = self._create(attributes, timeout, wait, auth=attributes['user_auth'], **kwargs)
+            return resource, True
+
+        elif state == 'present' and resource:
+            # Do not update resource
+            resource = self._update(attributes, timeout, wait, auth=attributes['user_auth'], **kwargs)
+            return resource, True
+
+        elif state == 'absent' and resource:
+            # Delete resource
+            self._delete(resource, attributes, timeout, wait, **kwargs)
+            return None, True
+
+        elif state == 'absent' and not resource:
+            # Do nothing
+            return None, False
+
+    def _find(self, attributes, **kwargs):
+        permissions = self.list_function(**attributes)
+        all_auth = list()
+        for permission in permissions:
+            all_auth.append(permission['self_auth'])
+            all_auth += permission['others_auths']
+        current_user = list(filter(lambda x: x['user_id'] == attributes['user_id'], permissions))
+        if len(current_user) > 1:
+            self.fail_json(msg='Found more than a single resource'
+                               ' which matches the given attributes.')
+        elif len(current_user) == 0:
+            return None
+        else:
+            return current_user[0]
+
+
+class SwrRepoPermissionModule(OTCModule):
+    argument_spec = dict(
+        namespace=dict(required=True),
+        repository=dict(required=True),
+        user_id=dict(required=True),
+        user_name=dict(required=True),
+        user_auth=dict(required=False,
+                       type='int',
+                       choices=[1, 3, 7],
+                       default=1),
+        state=dict(type='str', required=False,
+                   choices=['present', 'absent'],
+                   default='present'),
+    )
+    module_kwargs = dict(
+        supports_check_mode=True
+    )
+
+    def run(self):
+        service_name = "swr"
+        type_name = "repository_permissions"
+        session = getattr(self.conn, service_name)
+        create_function = getattr(session, 'create_{0}'.format(type_name))
+        delete_function = getattr(session, 'delete_{0}'.format(type_name))
+        update_function = getattr(session, 'update_{0}'.format(type_name))
+        list_function = getattr(session, 'repository_permissions'.format(type_name))
+        crud = dict(
+            create=create_function,
+            delete=delete_function,
+            find=None,
+            get=None,
+            list=list_function,
+            update=update_function,
+        )
+        sm = SwrRepoPermissionMachine(connection=self.conn,
+                                      sdk=self.sdk,
+                                      service_name=service_name,
+                                      type_name=type_name,
+                                      crud_functions=crud)
+        kwargs = {'state': self.params['state'],
+                  'attributes': dict((k, self.params[k]) for k in
+                                     ['namespace', 'repository', 'user_id', 'user_name']
+                                     if self.params[k] is not None)}
+        permission, is_changed = sm(check_mode=self.ansible.check_mode,
+                                    non_updateable_attributes=['namespace', 'repository', 'user_id', 'user_name'],
+                                    updateable_attributes=['auth'],
+                                    wait=False,
+                                    timeout=600,
+                                    **kwargs)
+        self.exit_json(permission=permission, changed=is_changed)
+
+
+def main():
+    module = SwrRepoPermissionModule()
+    module()
+
+
+if __name__ == "__main__":
+    main()
