2f463985a7f1c8c19338db5eef45e2a965a307a0
Sebastian Gode <sebastian.gode@telekom.de>
2024-05-31 08:12:08 +0000

    Fixed dark theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,6 +33,8 @@ extensions = [
     'sphinx_antsibull_ext'
 ]
 
+antsibull_ext_color_scheme = 'default-autodark'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
