11315001ba1a2f92d7da1786da2ad3b86dcfbf93
anton-sidelnikov <a.sidelnikov@t-systems.com>
2024-06-10 12:01:42 +0200

    ansible 2.17 change

--- tests/utils/sanity.sh
+++ tests/utils/sanity.sh
@@ -27,4 +27,17 @@ if [ -f "requirements.txt" ]; then
 fi
 
 cd "$TEST_DIR"
-ansible-test sanity --skip-test metaclass-boilerplate --skip-test future-import-boilerplate  --skip-test import --skip-test validate-modules  --skip-test compile --python 2.7
+SKIP_TESTS="--skip-test import --skip-test validate-modules  --skip-test compile --python 2.7"
+# Ansible-core 2.17 dropped support for the metaclass-boilerplate and future-import-boilerplate tests.
+ANSIBLE_VER=$(python3 -m pip show ansible-core | awk '$1 == "Version:" { print $2 }')
+ANSIBLE_MAJOR_VER=$(echo "$ANSIBLE_VER" | sed 's/^\([0-9]\)\..*/\1/g')
+SKIP_TESTS=""
+if [[ $ANSIBLE_MAJOR_VER -eq 2 ]]; then
+    ANSIBLE_MINOR_VER=$(echo "$ANSIBLE_VER" | sed 's/^2\.\([^\.]*\)\..*/\1/g')
+    if [[ $ANSIBLE_MINOR_VER -le 16 ]]; then
+        SKIP_TESTS="--skip-test metaclass-boilerplate --skip-test future-import-boilerplate --skip-test import --skip-test validate-modules  --skip-test compile --python 2.7"
+    fi
+fi
+
+ansible-test sanity -v \
+    $SKIP_TESTS
