aa2b1222561157ec596ebc150813150ce93cdf84
Sebastian Gode <sebastian.gode@telekom.de>
2025-02-05 14:13:38 +0000

    ims info 40% done

--- plugins/modules/ims_info.py
+++ plugins/modules/ims_info.py
@@ -370,40 +370,104 @@ UUID_PATTERN = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$', re.IGNORE
 
 class KMSInfoModule(OTCModule):
     argument_spec = dict(
-        name=dict(required=False),
-        key_state=dict(required=False, no_log=False),
+        name=dict(required=False, type='str'),
+        isregistered=dict(required=False, type='bool'),
+        imagetype=dict(required=False, type='str', choices=['gold', 'private', 'shared']),
+        whole_image=dict(required=False, type='bool'),
+        system_cmkid=dict(required=False, type='str'),
+        protected=dict(required=False, type='bool'),
+        visibility=dict(required=False, type='str', choices=['public', 'private', 'shared']),
+        owner=dict(required=False, type='str'),
+        status=dict(required=False, type='str', choices=['queued', 'saving', 'deleted', 'killed', 'active']),
+        container_format=dict(required=False, type='str', choices=['bare']),
+        disk_format=dict(required=False, type='str', choices=['zvhd2', 'vhd', 'zvhd', 'raw', 'qcow2']),
+        min_ram=dict(required=False, type='int'),
+        min_disk=dict(required=False, type='int'),
+        os_bit=dict(required=False, type='str', choices=['32', '64']),
+        platform=dict(required=False, type='str', choices=['Windows', 'Ubuntu', 'Red Hat', 'SUSE', 'CentOS', 'Debian', 'OpenSUSE', 'Oracle Linux', 'Fedora', 'CoreOS', 'EulerOS', 'Other']),
+        marker=dict(required=False, type='str'),
+        os_type=dict(required=False, type='str', choices=['Linux', 'Windows', 'Other']),
+        tag=dict(required=False, type='str'),
+        member_status=dict(required=False, type='str', choices=['accepted', 'rejected', 'pending']),
+        support_kvm=dict(required=False, type='bool'),
+        support_xen=dict(required=False, type='bool'),
+        support_largememory=dict(required=False, type='bool'),
+        support_diskintensive=dict(required=False, type='bool'),
+        support_highperformance=dict(required=False, type='bool'),
+        support_xen_gpu_type=dict(required=False, type='bool'),
+        support_kvm_gpu_type=dict(required=False, type='bool'),
+        support_xen_hana=dict(required=False, type='bool'),
+        support_kvm_infiniband=dict(required=False, type='bool'),
+        virtual_env_type=dict(required=False, type='str', choices=['FusionCompute', 'Ironic', 'DataImage', 'IsoImage']),
+        enterprise_project_id=dict(required=False, type='str')
     )
     module_kwargs = dict(
         supports_check_mode=True
     )
 
-    otce_min_version = '0.26.0'
+    otce_min_version = '0.31.14'
 
     def run(self):
 
         data = []
         query = {}
 
-        if self.params['key_state']:
-            query['key_state'] = self.params['key_state']
+        if self.params['name'] and self.params['id']:
+            self.fail_json(msg="Only specify either name or id, not both.")
 
         if self.params['name']:
-            if UUID_PATTERN.match(self.params['name']):
-                raw = self.conn.kms.get_key(self.params['name'])
-                if raw:
-                    dt = raw.to_dict()
-                    dt.pop('location')
-                    data.append(dt)
-            else:
-                raw = self.conn.kms.find_key(
-                    alias=self.params['name'],
-                    ignore_missing=True)
-                if raw:
-                    dt = raw.to_dict()
-                    dt.pop('location')
-                    data.append(dt)
+            query['name'] = self.params['name']
+        if self.params['imagetype']:
+            query['imagetype'] = self.params['imagetype']
+        if self.params['system_cmkid']:
+            query['system_cmkid'] = self.params['system_cmkid']
+        if self.params['visibility']:
+            query['visibility'] = self.params['visibility']
+        if self.params['owner']:
+            query['owner'] = self.params['owner']
+        if self.params['platform']:
+            query['platform'] = self.params['platform']
+        if self.params['os_type']:
+            query['os_type'] = self.params['os_type']
+        if self.params['tag']:
+            query['tag'] = self.params['tag']
+        if self.params['virtual_env_type']:
+            query['virtual_env_type'] = self.params['virtual_env_type']
+        if self.params.get('isregistered', False):
+            query['isregistered'] = self.params['isregistered']
+        if self.params.get('whole_image', False):
+            query['whole_image'] = self.params['whole_image']
+        if self.params.get('protected', False):
+            query['protected'] = self.params['protected']
+        if self.params.get('support_kvm', False):
+            query['support_kvm'] = self.params['support_kvm']
+        if self.params.get('support_xen', False):
+            query['support_xen'] = self.params['support_xen']
+        if self.params.get('support_largememory', False):
+            query['support_largememory'] = self.params['support_largememory']
+        if self.params.get('support_diskintensive', False):
+            query['support_diskintensive'] = self.params['support_diskintensive']
+        if self.params.get('support_highperformance', False):
+            query['support_highperformance'] = self.params['support_highperformance']
+        if self.params.get('support_xen_gpu_type', False):
+            query['support_xen_gpu_type'] = self.params['support_xen_gpu_type']
+        if self.params.get('support_kvm_gpu_type', False):
+            query['support_kvm_gpu_type'] = self.params['support_kvm_gpu_type']
+        if self.params.get('support_xen_hana', False):
+            query['support_xen_hana'] = self.params['support_xen_hana']
+        if self.params.get('support_kvm_infiniband', False):
+            query['support_kvm_infiniband'] = self.params['support_kvm_infiniband']
+        if self.params.get('min_ram'):
+            query['min_ram'] = self.params['min_ram']
+        if self.params.get('min_disk'):
+            query['min_disk'] = self.params['min_disk']
+        if self.params['os_bit']:
+            query['os_bit'] = self.params['os_bit']
+        if self.params['enterprise_project_id']:
+            query['enterprise_project_id'] = self.params['enterprise_project_id']
+
         else:
-            for raw in self.conn.kms.keys(**query):
+            for raw in self.conn.ims.keys(**query):
                 dt = raw.to_dict()
                 dt.pop('location')
                 data.append(dt)
