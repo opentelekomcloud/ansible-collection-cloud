6a383805143ce23e272715a7b3310bcd442548fd
SebastianGode <70581801+SebastianGode@users.noreply.github.com>
2026-08-05 10:25:11 +0200

    Update as_config_info.py

--- plugins/modules/as_config_info.py
+++ plugins/modules/as_config_info.py
@@ -10,6 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# test
 
 DOCUMENTATION = '''
 ---
@@ -99,4 +100,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
